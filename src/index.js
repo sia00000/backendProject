0acebd194a2a55453ef7dd77bc8b31da6bb0a343
Sheetal Gupta <155147431+sheetalsia@users.noreply.github.com>
2024-12-17 14:43:04 +0530

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import connectDB from "./DB/index.js";
+import "dotenv/config";
+
+connectDB();
+
+/*import mongoose from "mongoose";
+import express from "express";
+import { DB_NAME } from "./constant.js";
+import "dotenv/config";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", () => {
+      console.log("ERRR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error(`Error:`, error);
+    throw error;
+  }
+})();
+*/
